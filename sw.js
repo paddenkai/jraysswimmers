103cd541add4ac26edeb51f76142bac483052fbf
paddenkai <kai.padden11@gmail.com>
2026-04-08 00:33:47 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jrays-swimmers-v71';
+const CACHE_NAME = 'jrays-swimmers-v73';
 const SHELL_ASSETS = ['/', '/index.html', '/Azalea.otf'];
 
 self.addEventListener('install', event => {
