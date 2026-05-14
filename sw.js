3b499a76ed788e850cf5d14d348b15aca10e944e
paddenkai <kai.padden11@gmail.com>
2026-05-13 19:44:13 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jrays-swimmers-v126';
+const CACHE_NAME = 'jrays-swimmers-v128';
 const SHELL_ASSETS = ['/', '/index.html', '/Azalea.otf', '/PGA_Championship_Theme.mp3'];
 
 self.addEventListener('install', event => {
