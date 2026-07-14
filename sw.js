bc18db85f86fbaf46b1e9310ccf0d7f4c09f1f3d
paddenkai <kai.padden11@gmail.com>
2026-07-14 09:58:47 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'jrays-swimmers-v170';
-const SHELL_ASSETS = ['/', '/index.html', '/Azalea.otf'];
+const CACHE_NAME = 'jrays-swimmers-v174';
+const SHELL_ASSETS = ['/', '/index.html', '/Azalea.otf', '/PGA_Championship_Theme.mp3'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(SHELL_ASSETS)));
